188764ab1530946fd70f00843b6bd60cc30f16eb
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-07 14:17:06 -0400

    Add custom valiation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ApplicationRecord
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :required_phrase
+
+  def required_phrase
+    regex = /Won't Believe|Secret|Top \d+|Guess/
+    unless regex.match?(title)
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
